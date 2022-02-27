85f59d7e1ca04a5bc3db952dc9b20dee8e00187a
malbiruk <2601074@gmail.com>
2022-02-27 14:10:51 +0300

    updated dotfiles

--- auto_gtk.sh
+++ auto_gtk.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 time=`date "+%H"`
-echo $time
 
-if [[ $time < 8 || $time > 20 ]]; then
-  /home/klim/.config/i3/gtk-switch.py --apply td
-else
+if [[ $time > 5 ]] || [[ $time < 20 ]]; then
   /home/klim/.config/i3/gtk-switch.py --apply tl
+else
+  /home/klim/.config/i3/gtk-switch.py --apply td
 fi
